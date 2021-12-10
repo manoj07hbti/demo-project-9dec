852e948ff1f8e3b3020559d8125397594e155994
60manojkumar@gmail.com <Motivated@2019>
2021-12-10 20:11:21 +0530

    Initial commit

---
+++ src/DemoClass.java
@@ -0,0 +1,10 @@
+public class DemoClass {
+
+    public static void main(String[] args) {
+
+      System.out.println("This is my first Java program.... ");
+      System.out.println("Hello Java ");
+      System.out.println("This is third line ");
+    }
+
+}
