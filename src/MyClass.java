e55f970f801ec5a0700a802fcc0531703fae9fdc
60manojkumar@gmail.com <Motivated@2019>
2021-12-10 20:15:05 +0530

    new commit

--- src/MyClass.java
+++ src/MyClass.java
@@ -3,5 +3,6 @@ public class MyClass {
     public static void main(String[] args) {
 
         System.out.println("Hello java");
+        System.out.println("This is new line added ....");
     }
 }
