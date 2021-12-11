4beb1453bd9ef91a1d4d7f480045e37257bb7dbc
60manojkumar@gmail.com <Motivated@2019>
2021-12-11 09:22:23 +0530

    Variable Day1

---
+++ src/variable_demo/VariableDemo.java
@@ -0,0 +1,28 @@
+package variable_demo;
+
+public class VariableDemo {
+
+    public static void main(String[] args) {
+
+        //Syntax :
+        //Datatype variable_name=value;
+        String name="Java";// String values always will be in double court
+
+        String course="Microservice Developer";
+
+        System.out.println("Printing Variable name: " +name);
+        System.out.println("Printing Variable course: " +course);
+        //Datatype variable_name=value;
+        int age=25;
+
+        System.out.println("Printing Age :" +age);
+        //Datatype variable_name=value;
+        double price=35.5;
+
+        System.out.println("Printing price: " +price);
+        //Datatype variable_name=value;
+        boolean flag=true;
+        System.out.println("Printing flag "+flag);
+
+    }
+}
