b4f8de0d75d9e9960708c2acfbd3e99cad626ec6
60manojkumar@gmail.com <Motivated@2019>
2021-12-16 09:21:23 +0530

    Method Day2

--- src/class_and_object/Calculator.java
+++ src/class_and_object/Calculator.java
@@ -14,12 +14,42 @@ public class Calculator {
 
         System.out.println("Addition of a and b is : "+result);
     }
+    // Syntax : access_specifier return_type  method_name (parameter) {}
+    public void subtration(){
+
+        int a=5;
+        int b=3;
+        int result= a-b;
+
+        System.out.println("Subtraction  of a and b is : "+result);
+    }
+    // Syntax : access_specifier return_type  method_name (parameter) {}
+    public void multi(){
+
+        int a=5;
+        int b=3;
+        int result= a*b;
+
+        System.out.println("Multiplication   of a and b is : "+result);
+    }
+    // Syntax : access_specifier return_type  method_name (parameter) {}
+    public void divide(){
+
+        int a=5;
+        int b=3;
+        int result= a/b;
+
+        System.out.println("divide   of a and b is : "+result);
+    }
 
     public static void main(String[] args) {
 
         //Step 1
         Calculator obj= new Calculator();
         obj.addition();
+        obj.subtration();
+        obj.multi();
+        obj.divide();
 
     }
 }
