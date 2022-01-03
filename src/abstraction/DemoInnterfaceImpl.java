278cd36a45810582b17c9ef86bc36411cc4258db
60manojkumar@gmail.com <Motivated@2019>
2022-01-03 09:22:38 +0530

    Abstraction

---
+++ src/abstraction/DemoInnterfaceImpl.java
@@ -0,0 +1,18 @@
+package abstraction;
+
+public class DemoInnterfaceImpl implements DemoInterface {
+    @Override
+    public void m1() {
+
+    }
+
+    @Override
+    public void m2() {
+
+    }
+
+    @Override
+    public void m3() {
+
+    }
+}
